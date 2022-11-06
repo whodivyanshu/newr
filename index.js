9765a7fabf901885aa7b9352fae323d5f1cf4dda
divyanshu <divyanshulohar2002@gmail.com>
2022-11-06 22:36:56 +0530

    hello

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+
+app.get("/", function(req,res){
+
+    res.sendFile(__dirname,"\index.html");
+
+    
+});
+
+
+app.listen(3000, function(){
+    console.log("server started at port 3000");
+})
